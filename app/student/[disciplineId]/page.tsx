7d97c0da62a49497f8417c2b71ab0bd95834b786
yashezoed <mr.efyx@mail.ru>
2025-05-29 15:21:16 +0300

    пытаюсь решит конфликт синхронных и асинхронных параметров 2

--- app/student/[disciplineId]/page.tsx
+++ app/student/[disciplineId]/page.tsx
@@ -4,29 +4,30 @@ import fetchTests from '@/lib/api/fetchTests';
 import isError from '@/lib/api/isError';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import TestDescription from '@/components/ui/TestDescription';
-import StudentLayout from '@/components/layout/studentLayout'; // исправлено
+import StudentLayout from '@/components/layout/studentLayout';
 import Tests from '@/components/pages/student/tests';
 
-export default async function Page({
-	params,
-	searchParams
-}: {
-	params: {
-		disciplineId: string; // обычно строка
-	};
-	searchParams: {
-		testId?: string;
-	};
-}) {
-	const disciplineId = Number(params.disciplineId);
-	const testId = searchParams.testId
+export default async function Page(
+    props: {
+        params: Promise<{
+            disciplineId: string;
+        }>;
+        searchParams: Promise<{
+            testId?: string;
+        }>;
+    }
+) {
+    const searchParams = await props.searchParams;
+    const params = await props.params;
+    const disciplineId = Number(params.disciplineId);
+    const testId = searchParams.testId
 		? Number(searchParams.testId)
 		: undefined;
 
-	const dataTests = await fetchTests(disciplineId);
-	const testInfo = await fetchLesson(testId || (dataTests as ITest[])[0].id);
+    const dataTests = await fetchTests(disciplineId);
+    const testInfo = await fetchLesson(testId || (dataTests as ITest[])[0].id);
 
-	return (
+    return (
 		<>
 			{!isError(testInfo) ? (
 				<StudentLayout title={testInfo.discipline.name}>
